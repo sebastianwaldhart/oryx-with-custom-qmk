4671a12b928a940b4b39dd8c1cea6514da628e3b
sebastian <sebastian.waldhart@gmail.com>
2025-01-07 18:35:40 +0100

    Use overrides and tap dance for special characters

--- Qjmnp/keymap.c
+++ Qjmnp/keymap.c
@@ -4,6 +4,20 @@
 #endif
 #include "eeprom.h"
 
+#define SP_OVERRIDE(K, X) ko_make_basic(MOD_BIT(KC_RIGHT_ALT), (K), (X))
+
+/*{.trigger_mods = MOD_BIT(KC_RIGHT_ALT), \*/
+  /*.layers                                = ~0, \*/
+  /*.suppressed_mods                       = MOD_BIT(KC_RIGHT_ALT), \*/
+  /*.options                               = ko_options_default, \*/
+  /*.negative_mod_mask                     = 0, \*/
+  /*.custom_action                         = NULL, \*/
+  /*.trigger                               = (K), \*/
+  /*.replacement                           = (X), \*/
+  /*.context                               = NULL, \*/
+  /*.enabled                               = NULL, \*/
+/*}*/
+
 enum planck_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
   ST_MACRO_0,
@@ -50,6 +64,14 @@ enum unicode_names {
   DBL_RIGHT,
 };
 
+enum tap_dance_names {
+  ACC_A,
+  ACC_E,
+  ACC_I,
+  ACC_O,
+  ACC_U,
+};
+
 const uint32_t PROGMEM unicode_map[] = {
   [UM_AE_LOWER] = 0x00E4,
   [UM_AE_UPPER] = 0x00C4,
@@ -78,6 +100,44 @@ const uint32_t PROGMEM unicode_map[] = {
   [DBL_LEFT] = 0x00AB,
   [DBL_RIGHT] = 0x00BB,
 };
+
+tap_dance_action_t tap_dance_actions[] = {
+  [ACC_A] = ACTION_TAP_DANCE_DOUBLE(UP(UM_AE_LOWER, UM_AE_UPPER), UP(MARK_A_LOWER, MARK_A_UPPER)),
+  [ACC_O] = ACTION_TAP_DANCE_DOUBLE(UP(UM_OE_LOWER, UM_OE_UPPER), UP(MARK_O_LOWER, MARK_O_UPPER)),
+  [ACC_U] = ACTION_TAP_DANCE_DOUBLE(UP(UM_UE_LOWER, UM_UE_UPPER), UP(MARK_U_LOWER, MARK_U_UPPER)),
+};
+
+const key_override_t override_a = SP_OVERRIDE(KC_A, TD(ACC_A));
+const key_override_t override_e = SP_OVERRIDE(KC_E, UP(MARK_E_LOWER, MARK_E_UPPER));
+const key_override_t override_i = SP_OVERRIDE(KC_I, UP(MARK_I_LOWER, MARK_I_UPPER));
+const key_override_t override_o = SP_OVERRIDE(KC_O, TD(ACC_O));
+const key_override_t override_u = SP_OVERRIDE(KC_U, TD(ACC_U));
+const key_override_t override_n = SP_OVERRIDE(KC_N, UP(NJ_LOWER, NJ_UPPER));
+const key_override_t override_s = SP_OVERRIDE(KC_S, UC(UM_SS));
+const key_override_t override_m = SP_OVERRIDE(KC_M, UC(MATH_MU));
+const key_override_t override_c = SP_OVERRIDE(KC_C, UC(CUR_EUR));
+const key_override_t override_esc = SP_OVERRIDE(KC_ESCAPE, UC(MATH_DEGREE));
+const key_override_t override_slash = SP_OVERRIDE(KC_SLASH, UP(INV_EX, INV_QU));
+const key_override_t override_quote = SP_OVERRIDE(KC_QUOTE, UC(DBL_LEFT));
+const key_override_t override_comma = SP_OVERRIDE(KC_COMMA, UC(DBL_RIGHT));
+
+const key_override_t *overrides[] = {
+  &override_a,
+  &override_e,
+  &override_i,
+  &override_o,
+  &override_u,
+  &override_n,
+  &override_s,
+  &override_m,
+  &override_c,
+  &override_esc,
+  &override_slash,
+  &override_quote,
+  &override_comma
+};
+
+const key_override_t **key_overrides = (const key_override_t**)overrides;
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
